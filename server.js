d50b9f5f4e5d0c8106007a93c76b8caa497d8add
Dima Zeklam <dema.ziklam@gmail.com>
2021-09-05 10:43:10 +0300

    commit

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 'use strict';
-const axios = require('axios');
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 require('dotenv').config();
+const mongoose = require('mongoose');
 const server = express();
 server.use(cors());
 server.use(express.json());
-const mongoose = require('mongoose');
+
 
 const PORT = process.env.PORT;
 const uniSchema = new mongoose.Schema({
