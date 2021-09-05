27f00b9acc3bc5af09b8283747f6b256d6809508
Dima Zeklam <dema.ziklam@gmail.com>
2021-09-05 10:22:24 +0300

    commit

--- server.js
+++ server.js
@@ -10,23 +10,23 @@ const mongoose = require('mongoose');
 
 const PORT = process.env.PORT;
 const uniSchema = new mongoose.Schema({
-    country:String,
-    name:String,
-    web_pages:String,
-    email:String
+    country: String,
+    name: String,
+    web_pages: String,
+    email: String
 })
-const unimodel = mongoose.model('uniDatabase',uniSchema);
+const unimodel = mongoose.model('uniDatabase', uniSchema);
 mongoose.connect(process.env.MONG_LINK, {
     useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useUnifiedTopology: true,
 })
 
 server.get('/', Home);
 server.get('/getApi', getApi);
-server.post('/addFav',addFav);
-server.get('/getFav',getFav);
-server.delete('/deleteFav/:uniID',deleteFav);
-server.put('/updateFav/:uniID',updateFav);
+server.post('/addFav', addFav);
+server.get('/getFav', getFav);
+server.delete('/deleteFav/:uniID', deleteFav);
+server.put('/updateFav/:uniID', updateFav);
 
 // Home function
 function Home(req, res) {
@@ -46,42 +46,42 @@ async function getApi(req, res) {
     let uniURL = `http://universities.hipolabs.com/search?country=${cityName}`;
     let uniData = await axios.get(uniURL);
     // console.log(uniData.data);
-let Data = uniData.data.map(item=>{
-    return new university(item);
-})
+    let Data = uniData.data.map(item => {
+        return new university(item);
+    })
     res.send(Data);
 }
 
 // server.post('/addFav',addFav);
 //  ------------- addFav -----------------
 async function addFav(req, res) {
-    let {country,name,web_pages,email}= req.body;
-    let Obj = new unimodel ({
-        country:country,
-        name:name,
-        web_pages:web_pages,
-        email:email
+    let { country, name, web_pages, email } = req.body;
+    let Obj = new unimodel({
+        country: country,
+        name: name,
+        web_pages: web_pages,
+        email: email
     })
     await Obj.save();
-   unimodel.find({email:email},(err,Data)=>{
-       if(err){
-           console.log(err);
-       }else{
-        // console.log('Data:',Data);
-           res.send(Data);
-       }
-   })
+    unimodel.find({ email: email }, (err, Data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            // console.log('Data:',Data);
+            res.send(Data);
+        }
+    })
 }
 
 // server.get('/getFav',getFav);
 //  -------------- getFav -----------
-function getFav(req, res){
+function getFav(req, res) {
     let email = req.query.email;
 
-    unimodel.find({email:email},(err,Data)=>{
-        if(err){
+    unimodel.find({ email: email }, (err, Data) => {
+        if (err) {
             console.log(err);
-        }else{
+        } else {
 
             // console.log('Data::::::::',Data);
             res.send(Data);
@@ -91,48 +91,48 @@ function getFav(req, res){
 }
 // server.delete('/deleteFav:uniID',deleteFav);
 // deleteFav
-function deleteFav(req,res){
-    let {email}=req.body;
-let uniID = req.params.uniID;
-unimodel.remove({_id:uniID},(err,Data)=>{
-    if(err){
-        console.log(err);
-    }else{
-        unimodel.find({email:email},(err,Data)=>{
-            if(err){
-                console.log(err);
-            }else{
-    
-                // console.log('Data2:::',Data);
-                res.send(Data);
-            }
-
-        })
-    }
-})
-}
-// server.put('updateFav:uniID',updateFav)
-function updateFav(req,res){
+function deleteFav(req, res) {
+    let { email } = req.body;
     let uniID = req.params.uniID;
-    let {country,name,web_pages,email} = req.body;
-
-    unimodel.findByIdAndUpdate(uniID,{country,name,web_pages,email},(err,Data)=>{
-        if(err){
+    unimodel.remove({ _id: uniID }, (err, Data) => {
+        if (err) {
             console.log(err);
-        }else{
-            unimodel.find({email:email},(err,Data)=>{
-                if(err){
+        } else {
+            unimodel.find({ email: email }, (err, Data) => {
+                if (err) {
                     console.log(err);
-                }else{
-        
-                    console.log('Data2:::',Data);
+                } else {
+
+                    // console.log('Data2:::',Data);
                     res.send(Data);
                 }
-    
+
             })
         }
     })
-    }
+}
+// server.put('updateFav:uniID',updateFav)
+function updateFav(req, res) {
+    let uniID = req.params.uniID;
+    let { country, name, web_pages, email } = req.body;
+
+    unimodel.findByIdAndUpdate(uniID, { country, name, web_pages, email }, (err, Data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            unimodel.find({ email: email }, (err, Data) => {
+                if (err) {
+                    console.log(err);
+                } else {
+
+                    console.log('Data2:::', Data);
+                    res.send(Data);
+                }
+
+            })
+        }
+    })
+}
 server.listen(PORT, () => {
     console.log("all is goooooood", PORT);
 })
